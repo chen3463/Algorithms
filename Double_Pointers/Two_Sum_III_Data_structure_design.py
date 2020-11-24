73ca72b45cc46f6d84d37abd966f89e994a0a104
chen3463 <cqm0802@hotmail.com>
2020-11-23 21:50:48 -0600

    Update Two_Sum_III_Data_structure_design.py

--- Double_Pointers/Two_Sum_III_Data_structure_design.py
+++ Double_Pointers/Two_Sum_III_Data_structure_design.py
@@ -43,3 +43,30 @@ class TwoSum:
                 return True
         
         return False    
+
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):
+        self.dict = {}
+        
+    def add(self, number):
+        # write your code here
+        if number in self.dict:
+            self.dict[number] += 1
+        else:
+            self.dict[number] = 1
+
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        # write your code here
+        for num in self.dict:
+            if value - num in self.dict and (value - num != num or self.dict[num] > 1):
+                return True
+        
+        return False 
