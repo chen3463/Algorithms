a060e4ba12e992c04a737101afc194cf25a7ae12
chen3463 <cqm0802@hotmail.com>
2020-11-27 21:53:26 -0600

    Create Two Sum - Closest to target.py

---
+++ Double_Pointers/Two Sum - Closest to target.py
@@ -0,0 +1,53 @@
+'''
+Given an array nums of n integers, find two integers in nums such that the sum is closest to a given number, target.
+
+Return the absolute value of difference between the sum of the two integers and the target.
+
+Example
+Example1
+
+Input:  nums = [-1, 2, 1, -4] and target = 4
+Output: 1
+Explanation:
+The minimum difference is 1. (4 - (2 + 1) = 1).
+Example2
+
+Input:  nums = [-1, -1, -1, -4] and target = 4
+Output: 6
+Explanation:
+The minimum difference is 6. (4 - (- 1 - 1) = 6).
+Challenge
+Do it in O(nlogn) time complexity.
+'''
+
+class Solution:
+    """
+    @param nums: an integer array
+    @param target: An integer
+    @return: the difference between the sum and the target
+    """
+    def twoSumClosest(self, nums, target):
+        # write your code here
+        if not nums:
+            return 
+        
+        nums.sort()
+        current = None 
+        left, right = 0, len(nums) - 1 
+        min_diff = None
+        while left < right:
+            two_sum = nums[left] + nums[right]
+            if two_sum == target:
+                return 0
+                
+            if not current or abs(two_sum - target) < abs(current - target):
+                current = two_sum
+                min_diff = abs(two_sum - target)
+              
+            if two_sum < target:
+                left += 1 
+                
+            else:
+                right -= 1 
+                
+        return min_diff
