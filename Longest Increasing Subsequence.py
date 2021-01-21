686e737d02de3ff7d1ee671f3e27b7e276428c8c
chen3463 <cqm0802@hotmail.com>
2021-01-20 20:20:58 -0600

    Create Longest Increasing Subsequence.py

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,52 @@
+/*
+Given a sequence of integers, find the longest increasing subsequence (LIS).
+
+You code should return the length of the LIS.
+
+Example
+Example 1:
+	Input:  [5,4,1,2,3]
+	Output:  3
+	
+	Explanation:
+	LIS is [1,2,3]
+
+
+Example 2:
+	Input: [4,2,4,5,3,7]
+	Output:  4
+	
+	Explanation: 
+	LIS is [2,4,5,7]
+Challenge
+Time complexity O(n^2) or O(nlogn)
+
+Clarification
+What's the definition of longest increasing subsequence?
+
+The longest increasing subsequence problem is to find a subsequence of a given sequence in which the subsequence's elements are in sorted order, lowest to highest, and in which the subsequence is as long as possible. This subsequence is not necessarily contiguous, or unique.
+
+https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+*/
+
+
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        # write your code here
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        max_len = 0
+        matrix = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    matrix[i] = max(matrix[i], matrix[j] + 1)
+            
+        return max(matrix)
